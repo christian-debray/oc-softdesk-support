927cc66df303fbec52718ee9adad016a9c250406
Christian Debray <christian.debray5@gmail.com>
2024-12-05 18:29:09 +0100

    change model

--- api/models.py
+++ api/models.py
@@ -2,6 +2,11 @@ from django.db import models
 from accounts.models import User
 
 
+class Contributor(models.Model):
+    user = models.OneToOneField(to=User, to_field="username", on_delete=models.CASCADE)
+    created_time = models.DateTimeField(auto_now_add=True)
+
+
 class Project(models.Model):
     class ProjectType(models.TextChoices):
         BACKEND = "BACKEND", "Backend"
@@ -13,9 +18,4 @@ class Project(models.Model):
     type = models.CharField(choices=ProjectType, null=False, blank=False, max_length=8)
     description = models.TextField(null=False, blank=True)
     created_time = models.DateTimeField(auto_now_add=True)
-
-
-class Contributor(models.Model):
-    user = models.OneToOneField(to=User, on_delete=models.CASCADE)
-    project = models.ForeignKey(to=Project, on_delete=models.CASCADE, related_name="author")
-    created_time = models.DateTimeField(auto_now_add=True)
+    author = models.ForeignKey(to=Contributor, on_delete=models.CASCADE)
